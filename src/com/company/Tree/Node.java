8cbac46afa1a1316c62ad7bf88fa084bc5c16c37
moneg <monegask1969@gmail.com>
2019-03-20 20:38:13 +0200

    Initial commit

---
+++ src/com/company/Tree/Node.java
@@ -0,0 +1,13 @@
+package com.company.Tree;
+
+public class Node {
+
+    public int data;
+    public Node left;
+    public Node right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
